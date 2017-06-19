c147c5beb11583a3b2f28ac8da32babb39324b73
Justin Jungst <jjungstprogramming@gmail.com>
2017-06-19 14:20:37 -0500

    migration fix

--- db/migrate/20170613163609_create_recipe_ingredients.rb
+++ db/migrate/20170613163609_create_recipe_ingredients.rb
@@ -1,6 +1,5 @@
 class CreateRecipeIngredients < ActiveRecord::Migration[5.0]
   def change
-    drop_table :recipe_ingredients
     create_table :recipe_ingredients do |t|
       t.integer :recipe_id
       t.integer :ingredient_id
